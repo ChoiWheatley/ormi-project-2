2fa294eac310545c99c2e56eac1fc767c4282243
ChoiWheatley <chltmdgus604@naver.com>
2023-07-25 09:07:35 +0000

    feat(modify): Enhace New, Modify

--- articles/views.py
+++ articles/views.py
@@ -1,12 +1,8 @@
-from typing import Any, Optional
-from urllib import request
-from django import http
-from django.db import models
-from django.http import HttpRequest, HttpResponse, HttpResponseForbidden, HttpResponseServerError
-from django.http.response import HttpResponse
-from django.shortcuts import redirect, render
-from django.urls import reverse, reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView, UpdateView
+from typing import Any, Dict
+from django.forms.models import BaseModelForm
+from django.http import HttpRequest, HttpResponse
+from django.urls import reverse_lazy
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from django.contrib.auth.mixins import LoginRequiredMixin, UserPassesTestMixin
 from articles.models import Article
 from articles.forms import ArticleForm
@@ -35,6 +31,14 @@ class New(LoginRequiredMixin, CreateView):
     template_name = "write.html"
     form_class = ArticleForm
     success_url = reverse_lazy("articles:list")
+    queryset = model.objects.all()
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """form.is_valid()가 통과하면 무조건 실행되는 메서드, save 하기 전에
+        여기에 `author` 정보를 추가해야 한다."""
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
 
 
 class Modify(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
@@ -43,6 +47,28 @@ class Modify(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
     model = Article
     template_name = "update.html"
     form_class = ArticleForm
+    success_url = reverse_lazy("articles:list")
+    queryset = model.objects.all()
+
+    def form_valid(self, form: BaseModelForm) -> HttpResponse:
+        """form.is_valid()가 통과하면 무조건 실행되는 메서드, save 하기 전에
+        여기에 `author` 정보를 추가해야 한다."""
+        form.instance.author = self.request.user
+
+        return super().form_valid(form)
+
+    def test_func(self) -> bool | None:
+        """author와 request 유저와 일치하여야 함."""
+        # TODO - impl
+        return True
+
+
+class Remove(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """글 삭제 확인페이지.
+    문서에 따르면 요청 메서드가 POST일 경우에만 삭제를 수행한다고 하니 참고바람."""
+
+    model = Article
+    template_name = "delete.html"
     success_url = reverse_lazy("articles:list")
     queryset = model.objects.all()
 
